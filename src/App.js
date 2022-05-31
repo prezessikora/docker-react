677a412b30780fb9b06f1dc9bf863f7e3228c38e
prezessikora <prezes.sikora@gmail.com>
2022-05-31 15:42:50 +0200

    Small change on feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>This Docker Node.js running on AWS</h1>
-        <p>Changed and redeployed!</p>
+        <p>Made on feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
